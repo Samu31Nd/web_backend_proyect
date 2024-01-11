ccab707d2dfd2c50ad69530eb236a877b116bf2b
SaeSnd <samuelslg03idk@gmail.com>
2024-01-11 14:12:32 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Proyecto backend para Aplicaciones Web
+
+## Recursos:
+
+[Convertir a PDF](https://mpdf.github.io/)
+[Información proyecto](https://jaortizr.github.io/recs20232.html)
--- counter.js
+++ counter.js
@@ -1,9 +1,12 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+
+  element.innerHTML = `<H1> Programa </H1>`
+
+  // let counter = 0
+  // const setCounter = (count) => {
+  //   counter = count
+  //   element.innerHTML = `count is ${counter}`
+  // }
+  // element.addEventListener('click', () => setCounter(counter + 1))
+  // setCounter(0)
 }
